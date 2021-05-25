dda50f21d3237088b91437b33bc76a8d2d3dc1cc
lara barbosa <larapattylprb@gmail.com>
2021-05-25 17:04:03 -0300

    task manager

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -37,6 +37,10 @@ class TasksController < ApplicationController
 
   private
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
